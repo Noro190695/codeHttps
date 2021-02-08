82d95dc9f844bacb902a2ac86359b28c47290fff
Noro <norojan56@mail.ru>
2021-02-08 20:03:41 +0400

    slider

--- script/swiper.js
+++ script/swiper.js
@@ -1,10 +1,28 @@
-var swiper = new Swiper('.swiper-container', {
+let swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     slidesPerView: 1,
     spaceBetween: 30,
     mousewheel: true,
     pagination: {
-        el: '.swiper-pagination',
+        el: '.swiper-pagination-v',
         clickable: true,
     },
 });
+
+let insetSwiper = new Swiper('.inset__swiper-container', {
+    slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination-h',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
