0bb8ddf6c9506c5719c959cb929031093e43d95a
Noro <norojan56@mail.ru>
2021-02-09 16:54:45 +0400

    scroll

--- script/script.js
+++ script/script.js
@@ -16,4 +16,23 @@
 
     Burger('.burger')
 
+    function scroll(selector) {
+        let el = document.querySelector(selector)
+        el.addEventListener('click', function (e)  {
+           let link = this.href.match(/#\w+/)[0];
+           
+           if (link === '#footer') {
+               el.href = '#main'
+               el.style = 'transform: rotate(180deg)';
+           }else{
+               el.href = '#footer'
+               el.style = 'transform: rotate(0deg)';
+           }
+      
+        })
+   
+    }
+
+    scroll('.scroll')
+
 })()
